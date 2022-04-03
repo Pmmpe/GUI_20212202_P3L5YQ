8d12947ada2d7713f02dd21cb7218c17708619db
Pmmpe <75170631+Pmmpe@users.noreply.github.com>
2022-04-04 00:26:08 +0200

    Game items and enemies created.

--- King_of_the_Hill/King_of_the_Hill/Model/NPC Types/ShieldBearer.cs
+++ King_of_the_Hill/King_of_the_Hill/Model/NPC Types/ShieldBearer.cs
@@ -1,7 +1,9 @@
 ﻿namespace King_of_the_Hill.Model.NPC_Types
 {
     using System.Drawing;
-    public class ShieldBearer : Npc
+    public class ShieldBearer : Npc //just and idea nees further disscussion.
+                                    //Could only be damaged from the opposite side to his shield
+                                    //or only from upward.
     {
         public ShieldBearer(double Health, double Armour, double Speed, Size gameArea, int itemRadius) : base(Health, Armour, Speed, gameArea, itemRadius)
         {
